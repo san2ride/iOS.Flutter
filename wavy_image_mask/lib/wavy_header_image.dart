4a463575fe91b61b71eb5f4f6a49b04244a2b863
Jason <jason@sphere.ms>
2018-03-21 14:56:50 -0600

    images.dart

---
+++ wavy_image_mask/lib/wavy_header_image.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class WavyHeaderImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Text('This is where our image will be');
+  }
+}
