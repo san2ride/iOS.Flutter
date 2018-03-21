612852b86576181f4a892c61b247ba37842c471d
Jason <jason@sphere.ms>
2018-03-21 01:38:53 -0600

    1st commit

--- loflutter/lib/main.dart
+++ loflutter/lib/main.dart
@@ -89,9 +89,6 @@ the summer toboggan run.
 
     return new MaterialApp(
       title: 'Flutter Layout',
-      theme: new ThemeData(
-        primarySwatch: Colors.purple,
-      ),
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('Top Lakes'),
@@ -110,55 +107,6 @@ the summer toboggan run.
           ],
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return new Scaffold(
-      appBar: new AppBar(
-        
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
